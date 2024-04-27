f1488e59584375fe7ebd2aa1ee35ce7b4ee16204
Breno-de-Angelo <brenodeangelo@gmail.com>
2024-04-27 20:48:24 -0300

    Implementation of generic list

--- src/list.c
+++ src/list.c
@@ -1,4 +1,5 @@
 #include "list.h"
+#include <stdlib.h>
 
 typedef struct Node Node;
 struct Node
@@ -12,19 +13,35 @@ struct List
 {
     Node *first;
     Node *last;
+    size_t element_size;
 };
 
 List *create_list()
 {
-
+    List *list = malloc(sizeof(List));
+    list->first = NULL;
+    list->last = NULL;
+    return list;
 }
 
 void append(List *list, void *data)
 {
-
+    Node *node = malloc(sizeof(Node));
+    node->data = malloc(list->element_size);
+    if (list->first == NULL)
+    {
+        // Empty list
+        list->first = list->last = node;
+        node->prev = node->next = NULL;
+        return;
+    }
+    // Non-empty list
+    node->prev = list->last;
+    list->last = list->last->next = node;
+    return;
 }
 
 void delete_list(List *list)
 {
-
+    free(list);
 }
