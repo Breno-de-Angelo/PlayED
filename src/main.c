abf3425f1a35aaf3fbae535dbf4ebb1868175330
Breno U. de Angelo <brenodeangelo@gmail.com>
2024-05-18 12:28:37 -0300

    ✨ Include output.h to the new file created output.c ♻️ Un-commented some lines of code in main.c that define and call a function for the output paths 📝 Create and include new header file output.h to keep the project files organized

--- src/main.c
+++ src/main.c
@@ -12,15 +12,15 @@ int main()
     char *friendship_file_path = "Facil/Entrada/amizade.txt";
     char *playlist_file_path = "Facil/Entrada/playlists.txt";
     char *playlists_directory = "Facil/Entrada/";
-    // char *output_refactored_list_file_path = "Saida/played-refatorada.txt";
-    // char *output_refactored_playlists_directory = "Saida/";
+    char *output_refactored_list_file_path = "Saida/played-refatorada.txt";
+    char *output_refactored_playlists_directory = "Saida/";
     // char *output_similarity_file_path = "Saida/similaridades.txt";
 
     read_friendship(friendship_file_path, users_list);
     read_playlists(playlist_file_path, playlists_directory, users_list);
     refactor_playlists(users_list);
-    // (users_list);
-    // (users_list);
+    (users_list);
+    (users_list);
     
     return 0;
 }
